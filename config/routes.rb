6e4e4232157019fd9bc02d4a52dc1d925d210393
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-06-19 12:24:41 +0000

    [start]3-25

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+   root to: "homes#top"
+  devise_for :users
+  get "/homes/about" => "homes#about", as: "about"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :books, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit]
+
+end
